7cbd38c7e26efd658529414cca534d7998f3be84
mtaweela <mohamed.taweela56@gmail.com>
2018-09-25 11:01:32 +0200

    combine multible reducers

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
-import { createStore } from "redux";
+import { combineReducers , createStore } from "redux";
 
-// reducer
-const reducer = function(state, action) {
-    if (action.type === "INC") {
-        return state + action.payload;
-    }
-
-    if (action.type === "DEC") {
-        return state - action.payload;
+// reducers
+const userReducer = (state = {}, action) => {
+    switch(action.type) {
+        case "CHANGE_NAME": {
+            state = {...state, name:action.payload}
+            break;
+        }
+        case "CHANGE_AGE": {
+            state = {...state, age:action.payload}
+            break;
+        }
     }
 
     return state;
 };
 
+const tweetsReducer = (state = [], action) => {
+    return state;
+};
+
+
+// combined reducers
+const reducers = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer
+});
+
+
 // store
-const store = createStore(reducer, 0);
+const store = createStore(reducers);
 
 store.subscribe(() => {
     console.log("store changed", store.getState())
 });
 
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 222});
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "DEC", payload: 3});
+store.dispatch({type: "CHANGE_NAME", payload: "will"});
+store.dispatch({type: "CHANGE_AGE", payload: 35});
+store.dispatch({type: "CHANGE_AGE", payload: 45});
