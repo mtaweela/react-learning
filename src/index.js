35f9b9d1b2046af0aed1d644f43fafcd35972266
mtaweela <mohamed.taweela56@gmail.com>
2018-09-25 14:21:54 +0200

    redux promises

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import axios from "axios";
+import promise from "redux-promise-middleware";
 const initialState = {
     fetching: false,
     fetched: false,
@@ -12,15 +13,15 @@ const initialState = {
 // reducer
 const reducer = function(state=initialState, action) {
     switch (action.type) {
-        case "FETCH_USER_START" : {
+        case "FETCH_USERS_PENDING" : {
             return {...state, fetching: true};
             break;
         }
-        case "FETCH_USER_ERROR" : {
+        case "FETCH_USERS_REJECTED" : {
             return {...state, fetching: false, error: action.payload};
             break;
         }
-        case "RECIEVE_USERS" : {
+        case "FETCH_USERS_FULIFILLED" : {
             return {...state, fetching: false, fetched: true, users: action.payload};
             break;
         }
@@ -30,18 +31,12 @@ const reducer = function(state=initialState, action) {
 };
 
 // middlewares
-const middleware = applyMiddleware( thunk, logger);
+const middleware = applyMiddleware(promise(), thunk, logger);
 
 // store
 const store = createStore(reducer, middleware);
 
-store.dispatch((dispatch) => {
-    dispatch({type: "FETCH_USER_START"});
-    axios.get("http://rest.learncode.academy/api/mohamed/users")
-        .then((res) => {
-            dispatch({type: "RECIEVE_USERS", payload: res.data});
-        })
-        .catch((err) => {
-            dispatch({type: "FETCH_USERS_ERROR", payload: err});
-        });
+store.dispatch({
+    type: "FOO",
+    payload: axios.get("http://rest.learncode.academy/api/mohamed/users")
 });
