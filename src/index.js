02c78e3b8dacf427532bc2694278f8827af52cad
mtaweela <mohamed.taweela56@gmail.com>
2018-09-25 10:36:43 +0200

    creating reducer, store, actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// reducer
+const reducer = function(state, action) {
+    if (action.type === "INC") {
+        return state + action.payload;
+    }
+
+    if (action.type === "DEC") {
+        return state - action.payload;
+    }
+
+    return state;
+};
+
+// store
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+    console.log("store changed", store.getState())
+});
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 222});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "DEC", payload: 3});
